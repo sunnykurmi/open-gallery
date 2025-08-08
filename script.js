1f9ff5a60750e793064f5eaa4b222c7eab5abd40
sunnykurmi <sunnykurmiskks@gmail.com>
2025-08-08 13:40:33 +0530

    new

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  smooth: true,
+  lerp: 0.1,
+});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+// Now your gsap.to / fromTo animations...
+
+
 gsap.set(".about_img_1", { zIndex: 2 })
 
-gsap.to(".about_img_2",{
+gsap.to(".about_img_2 img",{
     scrollTrigger:{
         trigger:".about_txt_box_2",
         start:"top bottom",
@@ -10,7 +28,7 @@ gsap.to(".about_img_2",{
     },
     scale:.9
 })
-gsap.to(".about_img_3",{
+gsap.to(".about_img_3 img",{
     scrollTrigger:{
         trigger:".about_txt_box_3",
         start:"top bottom",
@@ -56,7 +74,7 @@ gsap.to(".about_img_4",{
 
 
 
-gsap.fromTo(".about_img_2",{
+gsap.fromTo(".about_img_2 img",{
     scale:1.1},{
     scrollTrigger:{
         trigger:".about_txt_box_1",
@@ -66,7 +84,7 @@ gsap.fromTo(".about_img_2",{
     },
     scale:1,
 })
-gsap.fromTo(".about_img_3",{
+gsap.fromTo(".about_img_3 img",{
     scale:1.1},{
     scrollTrigger:{
         trigger:".about_txt_box_2",
@@ -76,7 +94,7 @@ gsap.fromTo(".about_img_3",{
     },
     scale:1,
 })
-gsap.fromTo(".about_img_4",{
+gsap.fromTo(".about_img_4 img",{
     scale:1.1},{
     scrollTrigger:{
         trigger:".about_txt_box_3",
@@ -85,4 +103,45 @@ gsap.fromTo(".about_img_4",{
         // markers:true
     },
     scale:1,
+})
+
+
+
+
+
+gsap.fromTo(".about_txt_box_1_inner",{
+    y:-180
+},{
+    scrollTrigger:{
+        trigger:".about_txt_box_1",
+        start:"top bottom",
+        scrub:true,
+        end:"bottom top",
+        // markers:true
+    },
+  y:100
+})
+gsap.fromTo(".about_txt_box_2_inner",{
+    y:-150
+},{
+    scrollTrigger:{
+        trigger:".about_txt_box_2",
+        start:"top bottom",
+        scrub:true,
+        end:"bottom top",
+        // markers:true
+    },
+  y:100
+})
+gsap.fromTo(".about_txt_box_3_inner",{
+    y:-120
+},{
+    scrollTrigger:{
+        trigger:".about_txt_box_3",
+        start:"top bottom",
+        scrub:true,
+        end:"bottom top",
+        // markers:true
+    },
+  y:70
 })
